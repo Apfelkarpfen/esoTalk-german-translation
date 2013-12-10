423ca6c7fa4a6b564ec3a745068ac81fb0f897bc
Apfelkarpfen <jens@datenlaube.net>
2013-12-10 22:34:44 +0100

    German version of definitions.Answers.php

---
+++ German/definitions.Answers.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// English Definitions for the Answers plugin.
+
+$definitions["Answer"] = "Antwort"; //Answer
+$definitions["Answered"] = "Beantwortet"; //Answered
+$definitions["This post answered my question"] = "Dieser Beitrag hat meine Frage beantwortet."; //This post answered my question
+$definitions["Answered by %s"] = "Beantwortet von %s"; //Answered by %s
+$definitions["See post in context"] = "Beitrag im Kontext betrachten"; //See post in context
+$definitions["Remove answer"] = "Antwort entfernen"; //Remove answer
